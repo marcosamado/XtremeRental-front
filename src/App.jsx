3a842d3d61359b35707ce737a2efe7987cb4220e
marcosamado <led_9dejulio@hotmail.com>
2023-10-20 00:03:37 -0300

    probando Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className="container ml-auto mr-auto flex flex-row justify-around flex-wrap  sm:flex-col gap-10 p-10 items-center">
+            <h1 className="text-2xl font-bold underline text-colorMain bg-gray-800 shadow-xl shadow-lime-600 rounded p-3 w-60 text-center">
+                Hello world!
+            </h1>
+            <h1 className="text-2xl font-bold underline text-colorMain bg-gray-800 shadow-xl shadow-line-600 rounded-md p-3 w-60 text-center">
+                Hello world!
+            </h1>
+            <h1 className="text-2xl font-bold underline text-colorMain bg-gray-800 shadow-xl shadow-lime-600 rounded-md p-3 w-60 text-center">
+                Hello world!
+            </h1>
+            <h1 className="text-2xl font-bold underline text-colorMain bg-gray-800 shadow-xl shadow-lime-600 rounded-md p-3 w-60 text-center">
+                Hello world!
+            </h1>
+            <h1 className="text-2xl font-bold underline text-colorMain bg-gray-800 shadow-xl shadow-lime-600 rounded-md p-3 w-60 text-center">
+                Hello world!
+            </h1>
+        </div>
+    );
 };
 
 export default App;
