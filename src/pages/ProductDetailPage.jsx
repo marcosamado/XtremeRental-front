94dc5b3276c54eb3fc63a1dd67d2129eec591130
nicolastoscano <nicolastoscano98@gmail.com>
2023-10-25 20:03:41 -0300

    footer terminado

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const ProductDetailPage = () => {
-    return <div>ProductDetailPage</div>;
+  return <div>ProductDetailPage</div>;
 };
 
 export default ProductDetailPage;
