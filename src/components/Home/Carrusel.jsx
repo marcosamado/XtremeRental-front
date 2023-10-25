3925eccb637b41da26e06ee01e9a9cf9161b4658
AylenCormick <aylucormick@gmail.com>
2023-10-25 00:31:50 -0300

    carrusel terminado, correccion de imagenes

---
+++ src/components/Home/Carrusel.jsx
@@ -0,0 +1,118 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import Card from "./Card";
+
+const cards = [
+    {
+        precio: 55000,
+        id: 1,
+        title: "Campera Avant 3 Gore-Tex® 3c Ski Negro",
+        Url: "https://www.ansilta.com/img/articulos/campera_avant_iii_2_imagen3.jpg",
+        tipo: "campera-nieve",
+    },
+    {
+        precio: 30000,
+        id: 2,
+        title: "Campera Avant 3 Gore-Tex® 3c Ski Gris",
+        Url: "https://www.ansilta.com/img/articulos/campera_avant_iii_imagen2.jpg",
+        tipo: "campera-nieve",
+    },
+    {
+        precio: 55000,
+        id: 3,
+        title: "Campera Avant 3 Gore-Tex® 3c Ski Rosa",
+        Url: "https://www.ansilta.com/img/articulos/anorak_avant_iii_4_imagen1.jpg",
+        tipo: "campera-nieve",
+    },
+    {
+        precio: 45000,
+        id: 4,
+        title: "Pantalón Slalom 3 Gore-Tex® 2c Ski",
+        Url: "https://www.ansilta.com/img/articulos/2022/09/pantalon_slalom_3_gore_tex_2c_ski_imagen9.jpg",
+        tipo: "pantalon-nieve",
+    },
+    {
+        precio: 84500,
+        id: 5,
+        title: "Pantalón Avant 4 Gore-Tex Pro Ski",
+        Url: "https://www.ansilta.com/img/articulos/2022/10/pantalon_avant_4_gore_tex_pro_ski_imagen6.jpg",
+        tipo: "pantalon-nieve",
+    },     
+    {
+        precio: 55000,
+        id: 6,
+        title: "Pantalón Slalom 3 Gore-Tex 2c Ski",
+        Url: "https://www.ansilta.com/img/articulos/pantalon_slalom_3_1_imagen1.jpg",
+        tipo: "pantalon-nieve",
+    },
+    {
+        precio: 3000,
+        id: 7,
+        title: "Cubre Mitón Mercedario",
+        Url: "https://www.ansilta.com/img/articulos/cubre_miton_mercedario_2_imagen1.jpg",
+        tipo: "guantes-nieve",
+    },
+    {
+        precio: 55000,
+        id: 8,
+        title: "Guantes Orion Windstopper® Soft Shell",
+        Url: "https://www.ansilta.com/img/articulos/guantes_orion_9_imagen4.jpg",
+        tipo: "guantes-nieve",
+    },
+    {
+        precio: 14500,
+        id: 9,
+        title: "Gafas Ansilta By Qualia - Aconcagua",
+        Url: "https://www.ansilta.com/img/articulos/2023/08/gafas_ansilta_by_qualia_aconcagua_imagen1.jpg",
+        tipo: "lentes-nieve",
+    },
+    {
+        precio: 18450,
+        id: 10,
+        title: "Gafas Ansilta By Qualia - Lanín",
+        Url: "https://www.ansilta.com/img/articulos/2023/08/gafas_ansilta_by_qualia_lanin_imagen1.jpg",
+        tipo: "lentes-nieve",
+    },
+];
+
+const Carrusel = () => {
+    return (
+        <Swiper
+            className="relative"
+            modules={[Navigation, Pagination, Autoplay]}
+            loop={true}
+            navigation={true}
+            pagination={{ clickable: true }}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                800: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 30,
+                },
+            }}
+        >
+            {cards.map((item) => (
+                <SwiperSlide key={item.id}>
+                    <Card {...item} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
+
+export default Carrusel;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import Carrusel from "../components/Carrusel";
+import Carrusel from "../components/Home/Carrusel";
 
 const HomePage = () => {
     return (
-        <div>
-            <section className="">
-                <h2 className="font-bold text-colorCalido text-center text-xl mb-3 mt-3 ">
+        <div className=""> 
+            <section className="ml-12 mr-12">
+                <h2 className="font-bold text-colorCalido text-center text-xl mb-5 mt-5 ">
                     CATEGORIAS
                 </h2>
-                <div className="flex flex-col md:gap-3">
-                    <div className="bg-[url('https://imagenes.elpais.com/resizer/r6ELDzucn7tMT9H9pkABv2TZWrA=/1960x1103/filters:focal(671x333:681x343)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/SRYQACBQBBCO7ADQSE3A3GUSBI.jpg')] w-full h-44 bg-cover bg-no-repeat">
+                <div className="md:flex flex-row md:gap-3 mb-6">
+                    <div className="bg-[url('https://imagenes.elpais.com/resizer/r6ELDzucn7tMT9H9pkABv2TZWrA=/1960x1103/filters:focal(671x333:681x343)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/SRYQACBQBBCO7ADQSE3A3GUSBI.jpg')] w-full h-44 md:h-[400px] bg-cover bg-no-repeat">
                         <h2 className="text-black font-bold text-xl tracking-widest p-2">
                             Equipo Acuatico
                         </h2>
                     </div>
-                    <div className="bg-[url('https://img.freepik.com/fotos-premium/climber-sube-montana-hermoso-paisaje-concepto-estilo-vida-activo-ai-generativa_888418-2107.jpg?w=1380')] w-full h-44 bg-cover bg-no-repeat">
+                    <div className="sm:bg-[url('https://img.freepik.com/foto-gratis/silueta-hombres-que-animan-hacer-senderismo-abren-brazos-al-soporte-amanecer-montana-viajes-estilo-vida-pasion-viajes-concepto-aventura-vacaciones-verano-al-aire-libre_1150-61321.jpg?w=1380&t=st=1698203728~exp=1698204328~hmac=ce02334570aa687c2317b176b4087f5af03a29b62faa3bd9b84530b58a875f07')] md:lg:xl:2xlhidden bg-[url('https://img.freepik.com/foto-gratis/impresionante-foto-joven-trepando-acantilado-dia-frio-brumoso_181624-18833.jpg?w=1380&t=st=1698203568~exp=1698204168~hmac=2d93204d1866adc568cb2b3207dd12bcdf70c84da5248e881f7e8a3f4435e46b')] w-full h-44 md:h-[400px] bg-cover bg-no-repeat">
                         <h2 className="text-black font-bold text-xl tracking-widest p-2">
                             Equipo Montaña-Trekking
                         </h2>
                     </div>
-                    <div className="bg-[url('https://www.sentirsebella.cl/wp-content/uploads/2017/05/nieve.jpg')] w-full h-44 bg-cover bg-no-repeat">
+                    <div className="bg-[url('https://www.sentirsebella.cl/wp-content/uploads/2017/05/nieve.jpg')] w-full h-44 md:h-[400px] bg-cover bg-no-repeat">
                         <h2 className="text-black font-bold text-xl tracking-widest p-2">
                             Equipo Nieve
                         </h2>
                     </div>
                 </div>
             </section>
-            <h2 className="font-bold text-colorCalido text-center text-xl mb-3 mt-6 ">
+
+            <h2 className="font-bold text-colorCalido text-center text-xl mt-6 mb-6">
                 PRODUCTOS DESTACADOS
             </h2>
+            <section className="mb-8">
+                <Carrusel></Carrusel>
+            </section>
 
-            <section>
+            <h2 className="font-bold text-colorCalido text-center text-xl mt-6 mb-6">
+                OFERTAS
+            </h2>
+            <section className="mb-8">
                 <Carrusel></Carrusel>
             </section>
         </div>
