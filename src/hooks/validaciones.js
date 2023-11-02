36a442d1fe0b1fb7e30a15e894119cd2e4ca9063
AylenCormick <aylucormick@gmail.com>
2023-11-01 22:49:50 -0300

    validaciones y estilado addproductpage

---
+++ src/hooks/validaciones.js
@@ -0,0 +1,14 @@
+
+// export function validarNombre(data, nombre) {
+    
+//     if (nombre.lenght < 4) {
+//         console.log("Ese nombre tiene menos de 3 caracteres");
+//     } else if (nombre.lenght > 20) {
+//         console.log("Ese nombre tiene mas de 20 caracteres");
+//     } else if (data.nombreProducto.find(nombre)) {
+//         console.log("Ese nombre ya esta utilizado");
+//     } else {
+//         console.log("ok");
+//     }
+
+// }
