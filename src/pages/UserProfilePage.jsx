7b7f44a498b766b7287d8ccfe86395670c21e6c1
marcosamado <led_9dejulio@hotmail.com>
2023-12-11 20:48:29 -0300

    agregando favoritos

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { PiUserSquare } from 'react-icons/pi';
 import { UserReservesCard } from '../components/products/UserReservesCard';
+import { ImCross } from 'react-icons/im';
 
 const UserProfilePage = () => {
     const user = JSON.parse(localStorage.getItem('user'));
-    const favoritos = JSON.parse(localStorage.getItem('favs')) || [];
+    const [favoritos, setFavoritos] = useState(
+        JSON.parse(localStorage.getItem('favs') || []),
+    );
 
     const [reserves, setReserves] = useState([]);
 
@@ -15,8 +18,13 @@ const UserProfilePage = () => {
             .catch((error) => console.error(error.message));
     }, []);
 
-    // console.log(reserves[0].producto[0].nombreProducto);
-    console.log(reserves);
+    const handleDeleteFav = (id) => {
+        let filteredFavoritos = favoritos.filter((item) => item.id !== id);
+        setFavoritos(filteredFavoritos);
+        localStorage.setItem('favs', JSON.stringify(filteredFavoritos));
+    };
+
+    useEffect(() => {}, [localStorage.getItem('favs')]);
 
     return (
         <div>
@@ -33,14 +41,14 @@ const UserProfilePage = () => {
                     Favoritos
                 </h2>
                 <ul className="flex flex-col  md:flex-row md:justify-center md:gap-4">
-                    {favoritos.map((producto) => (
+                    {favoritos?.map((producto) => (
                         <div key={producto.id}>
                             <div className="relative flex flex-col mb-10 h-auto mx-auto max-w-xs justify-center items-center bg-white border border-gray-700 rounded-lg shadow ">
                                 <div className="w-40 h-40 p-5">
                                     <img
                                         className="w-full h-full"
                                         // className="w-full h-60 p-8 rounded-t-lg"
-                                        src={producto.imagenes}
+                                        src={producto.imagenes[0]?.url}
                                         alt={producto.nombreProducto}
                                     />
                                 </div>
@@ -49,6 +57,13 @@ const UserProfilePage = () => {
                                     <h5 className="text-lg font-semibold tracking-tight text-gray-900">
                                         {producto.nombreProducto}
                                     </h5>
+                                    <button
+                                        onClick={() => {
+                                            handleDeleteFav(producto.id);
+                                        }}
+                                    >
+                                        <ImCross className="mt-5 text-colorCalido" />
+                                    </button>
                                 </div>
                             </div>
                         </div>
